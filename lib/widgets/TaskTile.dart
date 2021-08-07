831cd0ed565ce84e3282d4ceea02a819444738b1
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2021-08-07 06:25:44 -0500

    add checkbox function callback

--- lib/widgets/TaskTile.dart
+++ lib/widgets/TaskTile.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    Key key,
-  }) : super(key: key);
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+  void checkboxCallback(bool newValue) {
+    setState(() {
+      isChecked = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckbox(isChecked, checkboxCallback),
     );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function checkBoxCallBack;
+  TaskCheckbox(this.checkboxState, this.checkBoxCallBack);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(value: checkboxState, onChanged: checkBoxCallBack);
   }
 }
